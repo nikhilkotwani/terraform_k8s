7765d69c62fd1d6919217cbb80fdabd3fb91eb13
Nikhil Kotwani <nikhilkotwani@192-168-1-107.tpgi.com.au>
2020-05-06 17:16:09 +1000

    Created k8s cluster creation using terraform

---
+++ kubernetes.tf
@@ -0,0 +1,57 @@
+provider "kubernetes" {
+  load_config_file = false
+
+  host     = google_container_cluster.primary.endpoint
+  username = var.gke_username
+  password = var.gke_password
+
+  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "careful-result-276221"
+region     = "us-central1"
